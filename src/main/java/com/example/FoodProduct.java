91f6d9199c3d45d165dad351a888ae91e25accba
Anton Ahlqvist <anton.ahlqvist@iths.se>
2025-10-21 14:50:01 +0200

    Add FoodProduct overrides for shipping and details

--- src/main/java/com/example/FoodProduct.java
+++ src/main/java/com/example/FoodProduct.java
@@ -26,4 +26,14 @@ public class FoodProduct extends Product implements Perishable, Shippable {
     public BigDecimal weight() {
         return weight;
     }
+
+    @Override
+    public BigDecimal calculateShippingCost() {
+        return weight.multiply(BigDecimal.valueOf(50));
+    }
+
+    @Override
+    public String productDetails() {
+        return ("Something");
+    }
 }
